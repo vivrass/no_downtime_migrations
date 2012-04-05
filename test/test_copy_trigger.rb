ce060e0344c0cfaf242031e5a2d01e587da244f4
Martin Provencher <mprovencher86@gmail.com>
2012-04-05 17:04:08 -0400

    Add tests for CopyTrigger#remove_column_trigger and CopyTrigger#copy_column_trigger_name

--- test/test_copy_trigger.rb
+++ test/test_copy_trigger.rb
@@ -18,23 +18,19 @@ class TestCopyTrigger < Test::Unit::TestCase
 
     context "#create_copy_column" do
       should "execute a copy of the first column into the second" do
-        found = 0
-        @adapter.expects(:execute).at_least_once.with do |sql_query|
-          if sql_query == "UPDATE table_name SET second_column = first_column"
-            found += 1
-          end
-          true
+        expected_sql = "UPDATE table_name SET second_column = first_column"
+        assert_expects_params @adapter, :execute, expected_sql do
+          @adapter.create_copy_column("table_name", "first_column", "second_column")
         end
-
-        @adapter.create_copy_column("table_name", "first_column", "second_column")
-        assert_equal 1, found
       end
 
       [:insert, :update].each do |sql_method|
         context "sql #{sql_method}" do
           should "execute a old trigger drop" do
             expected_sql = "DROP TRIGGER IF EXISTS table_name_copy_column_first_column_second_column_on_#{sql_method};"
-            assert_expects_params @adapter, :execute, expected_sql
+            assert_expects_params @adapter, :execute, expected_sql do
+              @adapter.create_copy_column("table_name", "first_column", "second_column")
+            end
           end
 
           should "execute create the trigger" do
@@ -50,8 +46,56 @@ class TestCopyTrigger < Test::Unit::TestCase
               END
             SQL
 
-            assert_expects_params @adapter, :execute, expected_sql
+            assert_expects_params @adapter, :execute, expected_sql do
+              @adapter.create_copy_column("table_name", "first_column", "second_column")
+            end
           end
+        end
+      end
+    end
+
+    context "#remove_copy_column" do
+      [:insert, :update].each do |sql_method|
+        context "sql #{sql_method}" do
+          should "execute a trigger drop" do
+            expected_sql = "DROP TRIGGER IF EXISTS table_name_copy_column_first_column_second_column_on_#{sql_method};"
+            assert_expects_params @adapter, :execute, expected_sql do
+              @adapter.remove_copy_column("table_name", "first_column", "second_column")
+            end
+          end
+        end
+      end
+    end
+
+    context "#copy_column_trigger_name" do
+      setup do
+        @table = "table_name"
+        @source_column = "first_column"
+        @destination_column = "second_column"
+        @sql_method = "insert"
+      end
+
+      should "generate the expected name" do
+        expected = "#{@table}_copy_column_#{@source_column}_#{@destination_column}_on_#{@sql_method}"
+        received = @adapter.send(:copy_column_trigger_name, @table, @source_column, @destination_column, @sql_method)
+
+        assert_equal expected, received
+      end
+
+      context "expected name > 63 characters" do
+        setup do
+          @source_column = "source_long_name" * 10
+          @destination_column = "destination_long_name" * 10
+        end
+
+        should "generate a hash for table and column names" do
+          received = @adapter.send(:copy_column_trigger_name, @table, @source_column, @destination_column, @sql_method)
+          assert_match /copy_column_#{@sql_method}_\d+/, received
+        end
+
+        should "new name lenght <= 63 characters" do
+          received = @adapter.send(:copy_column_trigger_name, @table, @source_column, @destination_column, @sql_method)
+          assert received.size <= 63
         end
       end
     end
@@ -73,7 +117,7 @@ class TestCopyTrigger < Test::Unit::TestCase
       true
     end
 
-    @adapter.create_copy_column("table_name", "first_column", "second_column")
+    yield if block_given?
     assert_equal count, found, "Expected :\n#{expected_params}\nReceived :\n#{all_params.join("\n")}"
   end
 end
